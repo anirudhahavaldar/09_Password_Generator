81f7703cd23f20da95cd3a80366eea0280469e4e
Anirudha Havaldar <52499108+anirudhahavaldar@users.noreply.github.com>
2023-01-13 18:11:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,119 @@
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+]
+
+console.log(characters.length)
+
+const generateBtn = document.getElementById('generate-btn')
+const passwordOne = document.getElementById('password-one')
+const passwordTwo = document.getElementById('password-two')
+
+function generateCharacter() {
+  return characters[Math.floor(Math.random() * 91)]
+}
+
+function generatePassword() {
+  let password = ''
+  for (let i = 0; i < 15; i++) {
+    let character = generateCharacter()
+    password += character
+  }
+  return password
+}
+
+generateBtn.addEventListener('click', renderPassword)
+
+function renderPassword() {
+  passwordOne.textContent = generatePassword()
+  passwordTwo.textContent = generatePassword()
+}
